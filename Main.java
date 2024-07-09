bb47943e6f435151ff9d558463ae887b62e9db0b
hassanalzahrani <hassanalalzhrani23@gmail.com>
2024-07-09 11:59:12 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //2
+
+
+        System.out.println("enter any number");
+        int number = input.nextInt();
+        System.out.println(number);
+
+        System.out.println("enter String");
+        String word = input.next();
+        System.out.println(word);
+
+
+        ///==============================
+
+
+
+////////////////////////4
+        int [] numbers22= {10,-21,30,31,-25};
+
+        for (int number11:numbers22){
+            if (number11>=0){
+                System.out.println(number11 + " ppsitive");
+            }else {
+                System.out.println(number11+ "negative");
+            }
+        }
+
+        //////////////////////////////5
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
